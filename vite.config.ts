77d4b5b37c0fac89ca8b23c6fc918b69c8ed53d6
Etavioxy <Etavioxy@foxmail.com>
2023-07-11 17:40:55 +0800

    add ssr to vite.config, add wiki-route

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import ssr from 'vite-plugin-ssr/plugin'
+import md from 'vite-plugin-md'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      include: [/\.vue$/, /\.md$/]
+    }),
+    ssr({ prerender: true }),
+    md()
+  ],
 })
