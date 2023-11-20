5f0d9bb3b3bdef93e0ad9de9562accd324b26104
Etavioxy <Etavioxy@foxmail.com>
2023-11-20 14:11:01 +0800

    delete markdown-entry makefile

--- markdown-entry/gen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# A shell script to generate multiple files by calling a.py
-
-# Loop from $1 to $2
-for i in $(seq $1 $2)
-do
-  node randstr.js > ../assets/$i.md
-done
--- src/pages/wiki/index.page.server.ts
+++ src/pages/wiki/index.page.server.ts
@@ -2,7 +2,6 @@ import type { PageContextBuiltIn } from 'vite-plugin-ssr/types'
 import { importMd } from '@/utils/renderMarkdown'
 
 export { onBeforeRender }
-export { prerender }
 
 async function onBeforeRender(pageContext: PageContextBuiltIn) {
   const { id } = pageContext.routeParams
@@ -15,13 +14,4 @@ async function onBeforeRender(pageContext: PageContextBuiltIn) {
       }
     }
   }
-}
-
-import path from 'path'
-import needUpdate from '@/../assets/needUpdate.txt?raw'
-
-function prerender(): string[] {
-  const filenames = needUpdate.split('\n').filter((name) => name.startsWith('assets/') && name.endsWith('.md') );
-  console.log(filenames);
-  return filenames.map((name) => `/wiki/${path.basename(name, path.extname(name))}`);
 }
